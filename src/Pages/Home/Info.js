0731975c73e124b1ff57ed6e1bfcf8086bcf6f3d
Rafiul789 <89851417+Rafiul789@users.noreply.github.com>
2022-05-15 19:21:17 +0600

    details of services added

--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -8,7 +8,7 @@ const Info = () => {
     return (
         <div  className="grid grid-cols-1 lg:grid-cols-3 gap-5 px-12">
             <Infocard  cardTitle="Opening Hours" bgClass="bg-gradient-to-r from-secondary to-primary  "  img={clock} ></Infocard>
-            <Infocard cardTitle="Our Locations" bgClass="bg-accent"   img={marker} ></Infocard>
+            <Infocard cardTitle="Our Locations" bgClass="bg-gradient-to-r from-secondary to-primary"   img={marker} ></Infocard>
             <Infocard cardTitle="Contact Us" bgClass="bg-gradient-to-r from-secondary to-primary  " img={phone} ></Infocard>
         </div>
     );
--- src/Pages/Home/Service.js
+++ src/Pages/Home/Service.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Service = () => {
+const Service = ({service}) => {
 
 
 
@@ -8,14 +8,12 @@ const Service = () => {
     return (
         <div className="card w-96 bg-base-100 shadow-xl">
   <figure className="px-10 pt-10">
-    <img src="https://api.lorem.space/image/shoes?w=400&h=225" alt="Shoes" className="rounded-xl" />
+    <img src={service.img} alt="service" className="rounded-xl" />
   </figure>
   <div className="card-body items-center text-center">
-    <h2 className="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div className="card-actions">
-      <button className="btn btn-primary">Buy Now</button>
-    </div>
+    <h2 className="card-title">{service.name}</h2>
+    <p>{service.description}</p>
+    
   </div>
 </div>
     );
