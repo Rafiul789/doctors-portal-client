59a97de44be5829660ccf99fa35515239a1e6477
Rafiul789 <89851417+Rafiul789@users.noreply.github.com>
2022-05-15 14:05:03 +0600

    banner added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Pages/About/About';
 import Home from './Pages/Home/Home';
 import Navbar from './Pages/Shared/Navbar/Navbar';
 
@@ -8,7 +9,7 @@ function App() {
   return (
     <div >
       <Navbar></Navbar>
-    <Routes> <Route path='/' element={<Home></Home>}></Route>  </Routes>
+    <Routes> <Route path='/' element={<Home></Home>}></Route> <Route path='/about' element={<About></About>}></Route>  </Routes>
     </div>
   );
 }
