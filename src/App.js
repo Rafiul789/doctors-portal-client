70554d5fe4838e84ce86062ab821998b293c0efc
Rafiul789 <89851417+Rafiul789@users.noreply.github.com>
2022-05-15 12:50:48 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 
 import './App.css';
+import Navbar from './Pages/Shared/Navbar/Navbar';
 
 function App() {
   return (
-    <div className="App">
+    <div >
+      <Navbar></Navbar>
     <button class="btn btn-primary">Button</button>
     </div>
   );
