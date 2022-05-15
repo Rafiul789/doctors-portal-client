76aa38277be8982d129c5d8e31bf10e9fc35fed0
Rafiul789 <89851417+Rafiul789@users.noreply.github.com>
2022-05-15 12:58:23 +0600

    routes added to app js file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Pages/Home/Home';
 import Navbar from './Pages/Shared/Navbar/Navbar';
 
 function App() {
   return (
     <div >
       <Navbar></Navbar>
-    <button class="btn btn-primary">Button</button>
+    <Routes> <Route path='/' element={<Home></Home>}></Route>  </Routes>
     </div>
   );
 }
