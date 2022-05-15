2d8765af0a9a365e6bd6bec0117b70c5a128be6a
Rafiul789 <89851417+Rafiul789@users.noreply.github.com>
2022-05-15 14:48:32 +0600

    three cards added to home component

--- src/Pages/Home/Infocard.js
+++ src/Pages/Home/Infocard.js
@@ -1,15 +1,13 @@
 import React from 'react';
 
-const Infocard = () => {
+const Infocard = ({img}) => {
     return (
-        <div class="card lg:card-side bg-base-100 shadow-xl">
-        <figure> <img src="https://api.lorem.space/image/album?w=400&h=400" alt="Album"/></figure>
-        <div class="card-body">
-          <h2 class="card-title">New album is released!</h2>
+        <div className="card lg:card-side bg-base-100 shadow-xl bg-accent">
+        <figure> <img src={img}alt="Album"/></figure>
+        <div className="card-body">
+          <h2 className="card-title">New album is released!</h2>
           <p>Click the button to listen on Spotiwhy app.</p>
-          <div class="card-actions justify-end">
-            <button class="btn btn-primary">Listen</button>
-          </div>
+          
         </div>
       </div>
     );
