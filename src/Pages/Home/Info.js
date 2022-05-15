6fa86c2dd48559198c624ef7e7ba81d08a3d348f
Rafiul789 <89851417+Rafiul789@users.noreply.github.com>
2022-05-15 16:54:09 +0600

    cards info added

--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Infocard from './Infocard';
 import clock from '../../assets/icons/clock.svg';
+import marker from '../../assets/icons/marker.svg';
+import phone from '../../assets/icons/phone.svg';
 
 const Info = () => {
     return (
-        <div  className="grid grid-cols-1 lg:grid-cols-3 gap-5">
-            <Infocard img={clock} ></Infocard>
-            <Infocard img={clock} ></Infocard>
-            <Infocard img={clock} ></Infocard>
+        <div  className="grid grid-cols-1 lg:grid-cols-3 gap-5 px-12">
+            <Infocard  cardTitle="Opening Hours" bgClass="bg-gradient-to-r from-secondary to-primary  "  img={clock} ></Infocard>
+            <Infocard cardTitle="Our Locations" bgClass="bg-accent"   img={marker} ></Infocard>
+            <Infocard cardTitle="Contact Us" bgClass="bg-gradient-to-r from-secondary to-primary  " img={phone} ></Infocard>
         </div>
     );
 };
--- src/Pages/Home/Infocard.js
+++ src/Pages/Home/Infocard.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Infocard = ({img}) => {
+const Infocard = ({img,cardTitle,bgClass}) => {
     return (
-        <div className="card lg:card-side bg-base-100 shadow-xl bg-accent">
-        <figure> <img src={img}alt="Album"/></figure>
-        <div className="card-body">
-          <h2 className="card-title">New album is released!</h2>
+        <div className={`card lg:card-side bg-base-100 shadow-xl ${bgClass}`}>
+        <figure> <img className="pl-5" src={img}alt="Album"/></figure>
+        <div className="card-body text-white ">
+          <h2 className="card-title">{cardTitle}</h2>
           <p>Click the button to listen on Spotiwhy app.</p>
           
         </div>
