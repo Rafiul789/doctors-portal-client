d4c426084bbe70d4cab12f8cf55f972ed49c3cb4
Rafiul789 <89851417+Rafiul789@users.noreply.github.com>
2022-05-18 09:22:29 +0600

    requireauth added to app js file and signout added

--- src/App.js
+++ src/App.js
@@ -5,13 +5,18 @@ import About from './Pages/About/About';
 import Appointment from './Pages/Appointment/Appointment';
 import Home from './Pages/Home/Home';
 import Login from './Pages/Login/Login';
+import RequireAuth from './Pages/Login/RequireAuth';
 import Navbar from './Pages/Shared/Navbar/Navbar';
 
 function App() {
   return (
     <div className="max-w-7xl mx-auto" >
       <Navbar></Navbar>
-    <Routes> <Route path='/' element={<Home></Home>}></Route> <Route path='/about' element={<About></About>}></Route>  <Route path='/appointment' element={<Appointment></Appointment>}></Route> <Route path='/login' element={<Login></Login>}></Route> </Routes>
+    <Routes> <Route path='/' element={<Home></Home>}></Route> <Route path='/about' element={<About></About>}></Route>  <Route path="appointment" element={
+          <RequireAuth>
+            <Appointment />
+          </RequireAuth>
+        } /> <Route path='/login' element={<Login></Login>}></Route> </Routes>
     </div>
   );
 }
